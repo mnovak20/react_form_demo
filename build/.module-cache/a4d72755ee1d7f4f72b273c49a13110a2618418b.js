3e98b85f37dec37714de804b169b67f601994562
Mike <mnovak419@gmail.com>
2015-11-12 20:59:13 -0500

    react_form_demo

---
+++ build/.module-cache/a4d72755ee1d7f4f72b273c49a13110a2618418b.js
@@ -0,0 +1,32 @@
+
+
+var RadioOptionGroup = React.createClass({displayName: "RadioOptionGroup",
+	  propTypes: {
+	      name:React.PropTypes.string.isRequired,
+	      other:React.PropTypes.bool,
+	      options:React.PropTypes.array.isRequired
+	  },
+		onChange:function(event){
+			if(this.props.other){
+				this.refs.other.forceUpdate();
+			}
+		},
+		render: function(){
+			var name = this.props.name;
+			return(
+					React.createElement("div", {onChange: this.onChange}, 
+						this.props.options.map(function(option){
+
+							return (
+								React.createElement(RadioOption, {name: name, value: option.value, key: option.value}, 
+									option.label
+								)
+							)
+
+						}), 
+						
+						this.props.other && React.createElement(RadioOtherOption, {name: name, ref: "other"})
+					)
+				)
+		}
+})
